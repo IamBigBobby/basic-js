ef1a055c060770586e2c6cb7197b266deb1b87b4
Denis Svetleishii <denissvetleishii@outlook.com>
2023-12-18 22:07:26 +0400

    feat: add solution extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function repeater(str, options) {
+  console.log(str, options.repeatTimes, options.separator, options.addition, options.additionRepeatTimes, options.additionSeparator);
+
+  if (str === null) {
+    str = 'null';
+  }
+
+  if (str.toString() === '[object Object]') {
+    str = 'STRING_OR_DEFAULTSTRING_OR_DEFAULT';
+  }
+
+  for (let element in options) {
+    if (options[element] === null) {
+      options[element] = 'null';
+    }
+
+    if (typeof options[element] === "object" && !Array.isArray(options[element])) {
+      for (let part in options[element]){
+        element = options[element][part];
+      }
+    }
+  }
+
+  options.addition === undefined ? options.addition = '' : options.addition;
+  options.separator === undefined ? options.separator = '+' : options.separator;
+  options.additionSeparator === undefined ? options.additionSeparator = '|' : options.additionSeparator;
+
+  let baseStrArr = [];
+
+  if (options.repeatTimes === undefined) {
+    return `${str.toString()}${options.addition}`;
+  }
+
+ 
+  for (let i = 0; i < options.repeatTimes; i++) {
+    baseStrArr.push(str.toString());
+
+    if (options.additionRepeatTimes !== undefined) {
+      for (let j = 0; j < options.additionRepeatTimes; j++) {
+        if (j === options.additionRepeatTimes - 1) {
+          baseStrArr.push(options.addition);
+        } else {
+          baseStrArr.push(options.addition);
+          baseStrArr.push(options.additionSeparator);
+        }
+      }
+    }
+
+    baseStrArr.push(options.separator);
+  }
+
+  baseStrArr.pop();
+
+  return baseStrArr.join('');
 }
 
 module.exports = {
