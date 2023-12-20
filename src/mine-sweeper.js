5568098b13cbf7c84f926cac60df39016532b7ff
Denis Svetleishii <denissvetleishii@outlook.com>
2023-12-20 20:05:34 +0400

    feat: add solution mine sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,11 +23,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+// const matrix = [
+//   [true, false, false],
+//   [false, true, false],
+//   [false, false, false],
+// ]
+function minesweeper(matrix) {
+  let zeroArr = [];
 
+  for (let i = 0; i < matrix.length; i++) {
+    let rowArr = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      rowArr.push(0);
+    }
+    zeroArr.push(rowArr);
+  }
+
+  for (let i = 0; i < zeroArr.length; i++) {
+    for (let j = 0; j < zeroArr[i].length; j++) {
+      if (matrix[i][j] === true) {
+        if (i - 1 >= 0 && zeroArr[i - 1][j] !== undefined) {
+          zeroArr[i - 1][j]++;
+        }
+        if (j - 1 >= 0 && zeroArr[i][j - 1] !== undefined) {
+          zeroArr[i][j - 1]++;
+        }
+        if (j + 1 < zeroArr[i].length && zeroArr[i][j + 1] !== undefined) {
+          zeroArr[i][j + 1]++;
+        }
+        if (i + 1 < zeroArr.length && zeroArr[i + 1][j] !== undefined) {
+          zeroArr[i + 1][j]++;
+        }
+        if (i - 1 >= 0 && j - 1 >= 0 && zeroArr[i - 1][j - 1] !== undefined) {
+          zeroArr[i - 1][j - 1]++;
+        }
+        if (i - 1 >= 0 && j + 1 < zeroArr[i].length && zeroArr[i - 1][j + 1] !== undefined) {
+          zeroArr[i - 1][j + 1]++;
+        }
+        if (i + 1 < zeroArr.length && j - 1 >= 0 && zeroArr[i + 1][j - 1] !== undefined) {
+          zeroArr[i + 1][j - 1]++;
+        }
+        if (i + 1 < zeroArr.length && j + 1 < zeroArr[i].length && zeroArr[i + 1][j + 1] !== undefined) {
+          zeroArr[i + 1][j + 1]++;
+        }
+      }
+    }
+  }
+
+  return zeroArr;
+}
+// console.log(minesweeper(matrix));
 module.exports = {
   minesweeper
 };
